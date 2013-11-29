b5cc100b4612f943336132b7966494739f756224
bradthomas127 <bradthomas127@yahoo.com>
2013-11-29 18:44:53 -0500

    Fingerprint and build desc update
    Make BUILD_FINGERPRINT and PRIVATE_BUILD_DESC to reflect what we build
    isntead of using some static string from another device or some
    self-made imaginary string. This could be useful when looking at logs.

--- cm.mk
+++ cm.mk
@@ -9,5 +9,5 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := primoc
 PRODUCT_MODEL := One V
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_primoc BUILD_FINGERPRINT=virgin_mobile/htc_primoc/primoc:4.4/KRT16M/330937:user/user-debug PRIVATE_BUILD_DESC="4.4 KRT16M 330937 user-debug"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_primoc BUILD_FINGERPRINT=virgin_mobile/htc_primoc/primoc:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date +%Y%m%d%H%M%S):$(TARGET_BUILD_VARIANT)/user-debug PRIVATE_BUILD_DESC="$(PLATFORM_VERSION) $(BUILD_ID) $(shell date +%Y%m%d%H%M%S) user-debug"
 
